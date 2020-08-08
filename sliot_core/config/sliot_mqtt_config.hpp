b2c65ced154a02dc589f09e072efbd8bdc85c37f
Edward Sandor <ed@ewsandor.com>
2020-08-07 19:29:38 -0600

    Moving passwords to a common secrets file from indiviedual configs

---
+++ sliot_core/config/sliot_mqtt_config.hpp
@@ -0,0 +1,7 @@
+#ifndef _SLIOT_MQTT_CONFIG_HPP_
+#define _SLIOT_MQTT_CONFIG_HPP_
+
+#include "sliot_core_config.hpp"
+#include "sliot_secrets.hpp"
+
+#endif _SLIOT_MQTT_CONFIG_HPP_
---
+++ sliot_core/config/sliot_wifi_config.hpp
@@ -0,0 +1,7 @@
+#ifndef _SLIOT_WIFI_CONFIG_HPP_
+#define _SLIOT_WIFI_CONFIG_HPP_
+
+#include "sliot_core_config.hpp"
+#include "sliot_secrets.hpp"
+
+#endif //_SLIOT_WIFI_CONFIG_HPP_
